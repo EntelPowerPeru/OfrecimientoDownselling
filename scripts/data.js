68eaab0429d9dec52d300d23fa636672b6c2f06f
EntelPower <waxib37728@kixotic.com>
2023-02-22 16:39:58 -0500

    Entelv3

--- scripts/data.js
+++ scripts/data.js
@@ -77,9 +77,303 @@ export const lista_origen = [
             "Roaming: 4.5 GB para navegar y apps promo"
         ]
     }
+    {
+        "origen_id": 9,
+        "origen_nombre": "ENTEL POWER FAMILIAR 109.90",
+        "lista_caracteristicas": [
+            "Ilimitado con 140 GB de AV",
+            "RRSS y APPs se consumen de los GB de AV",
+            "Roaming: 5.5 GB para navegar y apps promo"
+        ]
+    }
+    {
+        "origen_id": 10,
+        "origen_nombre": "ENTEL POWER FAMILIAR 129.90",
+        "lista_caracteristicas": [
+            "Ilimitado con 150 GB de AV",
+            "RRSS y APPs se consumen de los GB de AV",
+            "Roaming: 8 GB para navegar y apps promo"
+        ]
+    }
+    {
+        "origen_id": 11,
+        "origen_nombre": "ENTEL POWER FAMILIAR 159.90",
+        "lista_caracteristicas": [
+            "Ilimitado con 160 GB de AV",
+            "RRSS y APPs se consumen de los GB de AV",
+            "Roaming: 12 GB para navegar y apps promo"
+        ]
+    }
+    {
+        "origen_id": 12,
+        "origen_nombre": "ENTEL POWER FAMILIAR 179.90",
+        "lista_caracteristicas": [
+            "Ilimitado con 165 GB de AV",
+            "RRSS y APPs se consumen de los GB de AV",
+            "Roaming: 14 GB para navegar y apps promo"
+        ]
+    }
+    {
+        "origen_id": 13,
+        "origen_nombre": "ENTEL POWER FAMILIAR 199.90",
+        "lista_caracteristicas": [
+            "Ilimitado con 170 GB de AV",
+            "RRSS y APPs se consumen de los GB de AV",
+            "Roaming: 16 GB para navegar y apps promo"
+        ]
+    }
+    {
+        "origen_id": 14,
+        "origen_nombre": "ENTEL POWER FAMILIAR 259.90",
+        "lista_caracteristicas": [
+            "Ilimitado con 180 GB de AV",
+            "RRSS y APPs se consumen de los GB de AV",
+            "Roaming: 20 GB para navegar y apps promo"
+        ]
+    }
+    {
+        "origen_id": 15,
+        "origen_nombre": "ENTEL POWER FAMILIAR 299.90",
+        "lista_caracteristicas": [
+            "Ilimitado con 190 GB de AV",
+            "RRSS y APPs se consumen de los GB de AV",
+            "Roaming: 24 GB para navegar y apps promo"
+        ]
+    }
 ];
 
 export const lista_destino = [
+    {
+        "destino_id": 9,
+        "destino_nombre": "ENTEL POWER FAMILIAR 129.90 SD N",
+        "origen_id": 15,
+        "opcion_id": 1,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 8 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 16 GB Roaming",
+            "Disminuye renta: S/170"
+        ]
+    },
+    {
+        "destino_id": 8,
+        "destino_nombre": "ENTEL POWER FAMILIAR 99.90 N",
+        "origen_id": 15,
+        "opcion_id": 2,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 19 GB Roaming",
+            "Disminuye renta: S/200"
+        ]
+    },
+
+
+
+    {
+        "destino_id": 9,
+        "destino_nombre": "ENTEL POWER FAMILIAR 129.90 SD N",
+        "origen_id": 14,
+        "opcion_id": 1,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 8 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 12 GB Roaming",
+            "Disminuye renta: S/130"
+        ]
+    },
+    {
+        "destino_id": 8,
+        "destino_nombre": "ENTEL POWER FAMILIAR 99.90 N",
+        "origen_id": 14,
+        "opcion_id": 2,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 15 GB Roaming",
+            "Disminuye renta: S/160"
+        ]
+    },
+
+
+
+    {
+        "destino_id": 9,
+        "destino_nombre": "ENTEL POWER FAMILIAR 129.90 SD N",
+        "origen_id": 13,
+        "opcion_id": 1,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 8 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 8 GB Roaming",
+            "Disminuye renta: S/70"
+        ]
+    },
+    {
+        "destino_id": 8,
+        "destino_nombre": "ENTEL POWER FAMILIAR 99.90 N",
+        "origen_id": 13,
+        "opcion_id": 2,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 11 GB Roaming",
+            "Disminuye renta: S/100"
+        ]
+    },
+
+
+
+    {
+        "destino_id": 9,
+        "destino_nombre": "ENTEL POWER FAMILIAR 129.90 SD N",
+        "origen_id": 12,
+        "opcion_id": 1,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 8 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 6 GB Roaming",
+            "Disminuye renta: S/50"
+        ]
+    },
+    {
+        "destino_id": 8,
+        "destino_nombre": "ENTEL POWER FAMILIAR 99.90 N",
+        "origen_id": 12,
+        "opcion_id": 2,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 11 GB Roaming",
+            "Disminuye renta: S/80"
+        ]
+    },
+
+
+
+    {
+        "destino_id": 9,
+        "destino_nombre": "ENTEL POWER FAMILIAR 129.90 SD N",
+        "origen_id": 11,
+        "opcion_id": 1,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 8 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 4 GB Roaming",
+            "Disminuye renta: S/30"
+        ]
+    },
+    {
+        "destino_id": 8,
+        "destino_nombre": "ENTEL POWER FAMILIAR 99.90 N",
+        "origen_id": 11,
+        "opcion_id": 2,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 7 GB Roaming",
+            "Disminuye renta: S/60"
+        ]
+    },
+
+
+
+    {
+        "destino_id": 8,
+        "destino_nombre": "ENTEL POWER FAMILIAR 99.90 SD N",
+        "origen_id": 10,
+        "opcion_id": 1,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 3 GB Roaming",
+            "Disminuye renta: S/30"
+        ]
+    },
+    {
+        "destino_id": 7,
+        "destino_nombre": "ENTEL POWER FAMILIAR 89.90 N",
+        "origen_id": 10,
+        "opcion_id": 2,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 3.5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 3 GB Roaming",
+            "Disminuye renta: S/40"
+        ]
+    },
+
+
+
+    {
+        "destino_id": 8,
+        "destino_nombre": "ENTEL POWER FAMILIAR 99.90 SD N",
+        "origen_id": 9,
+        "opcion_id": 1,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 0.5 GB Roaming",
+            "Disminuye renta: S/10"
+        ]
+    },
+    {
+        "destino_id": 7,
+        "destino_nombre": "ENTEL POWER FAMILIAR 89.90 N",
+        "origen_id": 9,
+        "opcion_id": 2,
+        "lista_ventajas": [
+            "GB Ilimitados de AV",
+            "RRSS y APPs ilimitado",
+            "Roaming: 3.5 GB para navegar y apps promocionales"
+        ],
+        "lista_desventajas": [
+            "Pierde: 2 GB Roaming",
+            "Disminuye renta: S/40"
+        ]
+    },
+
+
+
     {
         "destino_id": 7,
         "destino_nombre": "ENTEL POWER FAMILIAR 89.90 SD N",
